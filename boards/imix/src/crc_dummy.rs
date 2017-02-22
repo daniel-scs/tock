9927c21f56ac5bed385875fea8ab14e4aa6392e4
Daniel B. Giffin <daniel@beech-grove.net>
2017-02-22 11:38:48 -0800

    blink better

--- boards/imix/src/crc_dummy.rs
+++ boards/imix/src/crc_dummy.rs
@@ -49,21 +49,20 @@ pub fn crc_test_begin() {
     }
 }
 
-fn blink_loop(n: u8) {
-    blink_n(n, 0);
+fn blink_loop(n: u32) {
+    loop {
+        blink_n(n, 1);
+    }
 }
 
-fn blink_n(n: u8, times: u32) {
+fn blink_n(n: u32, mut times: u32) {
     unsafe {
         // blink the panic signal
         let led = &sam4l::gpio::PC[10];
         led.enable_output();
 
-        let mut i = times;
-        while times == 0 || i > 0 {
-            if times > 0 {
-                i -= 1;
-            }
+        while times > 0 {
+            times -= 1;
 
             for _ in 0..n {
                 for _ in 0..1000000 {
