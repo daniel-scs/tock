d0edc30f7241b0732ec52501629ea0d288d7ebcb
Daniel B. Giffin <daniel@beech-grove.net>
2017-03-21 14:03:31 -0700

    it works

--- userland/examples/crc/main.c
+++ userland/examples/crc/main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
-#include <led.h>
+#include <stdlib.h>
 #include <timer.h>
 #include <crc.h>
 
-void blinken(uint32_t rate);
 void finished(int val, int v1, int v2, void *data);
 
 int main(void) {
@@ -12,40 +10,39 @@ int main(void) {
 
   if (!crc_exists()) {
     printf("CRC driver does not exist\n");
-    goto fail;
+    exit(1);
   }
 
   v = crc_version();
   if (v != 0x00000202) {
     printf("CRC version unexpected: %lu\n", v);
-    goto fail;
+    exit(1);
   }
 
   if (crc_init() != 0) {
     printf("CRC init failed\n");
-    goto fail;
+    exit(1);
   }
 
   if (crc_subscribe(finished, 0) !=0) {
     printf("CRC subscribe failed\n");
-    goto fail;
+    exit(1);
   }
 
   if (crc_compute() != 0) {
     printf("CRC compute-request failed\n");
-    goto fail;
+    exit(1);
   }
 
   if (crc_compute() != EBUSY) {
     printf("unexpected result of overlapping CRC compute-request\n");
+    exit(1);
   }
 
-  printf("CRC SUCCESS\n");
-  blinken(1000);
-
-fail:
-  printf("CRC FAIL\n");
-  blinken(100);
+  printf("Waiting for CRC results ...\n");
+  while(1) {
+    yield();
+  }
 }
 
 void finished(int val, __attribute__((unused)) int v1,
@@ -54,19 +51,7 @@ void finished(int val, __attribute__((unused)) int v1,
   printf("CRC finished: %8lx\n", (uint32_t) val);
 
   delay_ms(1000);
-
   if (crc_compute() != 0) {
     printf("additional CRC compute-request failed\n");
-  }
-}
-
-__attribute__((noreturn)) void
-blinken(uint32_t rate)
-{
-  while(1) {
-    led_on(0);
-    delay_ms(rate);
-    led_off(0);
-    delay_ms(rate);
   }
 }
