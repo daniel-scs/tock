20001600a9025d87847ebc45f5930acba357051e
Daniel B. Giffin <daniel@beech-grove.net>
2017-03-23 15:12:47 -0700

    procid

--- userland/examples/crc/main.c
+++ userland/examples/crc/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <timer.h>
 #include <crc.h>
+#include <rng.h>
 
 struct test_case {
   enum crc_polynomial poly;
@@ -29,8 +30,16 @@ void receive_result(int, int, int, void *);
 
 bool completed;
 
+uint32_t procid;
+
 int main(void) {
   int r;
+
+  // Get a random number to distinguish this app instance
+  if ((r = rng_sync((uint8_t *) &procid, 4, 4)) != 4) {
+    printf("RNG failure\n");
+    exit(1);
+  }
 
   if (!crc_exists()) {
     printf("CRC driver does not exist\n");
@@ -48,28 +57,31 @@ int main(void) {
     exit(1);
   }
 
-  for (test_index = 0; test_index < n_test_cases; test_index++) {
-    struct test_case *t = &test_cases[test_index];
+  while (1) {
+    for (test_index = 0; test_index < n_test_cases; test_index++) {
+      struct test_case *t = &test_cases[test_index];
 
-    printf("Requesting test case %d (length %d) ...\n",
-           test_index, strlen(t->input));
+      printf("Requesting test case %d (length %d) ...\n",
+             test_index, strlen(t->input));
 
-    if ((r = crc_set_buffer(t->input, strlen(t->input))) != 0) {
-      printf("CRC set-buffer failed: %d\n", r);
-      exit(1);
+      if ((r = crc_set_buffer(t->input, strlen(t->input))) != 0) {
+        printf("CRC set-buffer failed: %d\n", r);
+        exit(1);
+      }
+
+      completed = false;
+      if ((r = crc_compute(t->poly)) != 0) {
+        printf("CRC compute-request failed: %d\n", r);
+        exit(1);
+      }
+
+      printf("Waiting for CRC results ...\n");
+      yield_for(&completed);
     }
 
-    completed = false;
-    if ((r = crc_compute(t->poly)) != 0) {
-      printf("CRC compute-request failed: %d\n", r);
-      exit(1);
-    }
-
-    printf("Waiting for CRC results ...\n");
-    yield_for(&completed);
+    printf("\n\n");
+    delay_ms(1000);
   }
-
-  printf("Finished\n");
 }
 
 void receive_result(int status, int v1,
@@ -80,7 +92,7 @@ void receive_result(int status, int v1,
 
   struct test_case *t = &test_cases[test_index];
 
-  printf("-> Case %d: ", test_index);
+  printf("[%8lx] Case %d: ", procid, test_index);
   if (status == SUCCESS) {
     printf("result=%8lx ", result);
     if (result == t->output)
