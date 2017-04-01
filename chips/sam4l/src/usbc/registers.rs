aac2ce67fd9cc9b48b924e0c5be37964866a9c7c
Daniel B. Giffin <daniel@beech-grove.net>
2017-03-31 19:54:53 -0700

    register experiments

--- chips/sam4l/src/usbc/registers.rs
+++ chips/sam4l/src/usbc/registers.rs
@@ -2,62 +2,110 @@
 
 use core::convert::Into;
 use core::ops::{BitOr, Not};
+use core::marker::PhantomData;
 
 // A memory-mapped register
-pub struct Reg(*mut u32);
+#[derive(Copy, Clone)]
+pub struct Reg<T> {
+    addr: *mut u32,
+    phantom: PhantomData<*mut T>,
+}
 
-impl Reg {
-    pub fn read(self) -> u32 {
-        unsafe { ::core::ptr::read_volatile(self.0) }
+impl<T: From<u32> + Into<u32>> Reg<T> {
+    pub const fn new(addr: *mut u32) -> Self {
+        Reg { addr: addr, phantom: PhantomData }
     }
 
-    pub fn write<U: Into<u32>>(self, n: U) {
-        unsafe { ::core::ptr::write_volatile(self.0, Into::into(n)); }
+    #[inline]
+    pub fn read(self) -> T {
+        unsafe { From::from(::core::ptr::read_volatile(self.addr)) }
+    }
+
+    #[inline]
+    pub fn write(self, n: T) {
+        unsafe { ::core::ptr::write_volatile(self.addr, Into::into(n)); }
     }
 }
 
 // A write-only memory-mapped register
-pub struct RegW(*mut u32);
+pub struct RegW<T> {
+    addr: *mut u32,
+    phantom: PhantomData<*mut T>,
+}
 
-impl RegW {
-    pub fn write(self, n: u32) {
-        unsafe { ::core::ptr::write_volatile(self.0, n); }
+impl<T: From<u32> + Into<u32>> RegW<T> {
+    pub const fn new(addr: *mut u32) -> Self {
+        RegW { addr: addr, phantom: PhantomData }
+    }
+
+    #[inline]
+    pub fn write(self, n: T) {
+        unsafe { ::core::ptr::write_volatile(self.addr, Into::into(n)); }
     }
 }
 
 // A read-only memory-mapped register
-pub struct RegR(*const u32);
+pub struct RegR<T> {
+    addr: *const u32,
+    phantom: PhantomData<*mut T>,
+}
 
-impl RegR {
-    pub fn read(self) -> u32 {
-        unsafe { ::core::ptr::read_volatile(self.0) }
+impl<T: From<u32> + Into<u32>> RegR<T> {
+    pub const fn new(addr: *const u32) -> Self {
+        RegR { addr: addr, phantom: PhantomData }
+    }
+
+    #[inline]
+    pub fn read(self) -> T {
+        unsafe { From::from(::core::ptr::read_volatile(self.addr)) }
     }
 }
 
 // An array of memory-mapped registers
-pub struct Regs(*mut u32);
+pub struct Regs<T> {
+    addr: *mut u32,
+    phantom: PhantomData<*mut T>,
+}
 
-impl Regs {
-    pub fn n(&self, index: u32) -> Reg {
-        unsafe { Reg(self.0.offset(index as isize)) }
+impl<T: From<u32> + Into<u32>> Regs<T> {
+    pub const fn new(addr: *mut u32) -> Self {
+        Regs { addr: addr, phantom: PhantomData }
+    }
+
+    pub fn n(&self, index: u32) -> Reg<T> {
+        unsafe { Reg::new(self.addr.offset(index as isize)) }
     }
 }
 
 // An array of write-only memory-mapped registers
-pub struct RegsW(*mut u32);
+pub struct RegsW<T> {
+    addr: *mut u32,
+    phantom: PhantomData<*mut T>,
+}
 
-impl RegsW {
-    pub fn n(&self, index: u32) -> RegW {
-        unsafe { RegW(self.0.offset(index as isize)) }
+impl<T: From<u32> + Into<u32>> RegsW<T> {
+    pub const fn new(addr: *mut u32) -> Self {
+        RegsW { addr: addr, phantom: PhantomData }
+    }
+
+    pub fn n(&self, index: u32) -> RegW<T> {
+        unsafe { RegW::new(self.addr.offset(index as isize)) }
     }
 }
 
 // An array of read-only memory-mapped registers
-pub struct RegsR(*mut u32);
+pub struct RegsR<T> {
+    addr: *const u32,
+    phantom: PhantomData<*const T>,
+}
 
-impl RegsR {
-    pub fn n(&self, index: u32) -> RegR {
-        unsafe { RegR(self.0.offset(index as isize)) }
+impl<T: From<u32> + Into<u32>> RegsR<T> {
+    pub const fn new(addr: *const u32) -> Self {
+        RegsR { addr: addr, phantom: PhantomData }
+    }
+
+    pub fn n(&self, index: u32) -> RegR<T> {
+        unsafe { RegR::new(self.addr.offset(index as isize)) }
     }
 }
 
@@ -67,36 +115,43 @@ const USBC_BASE: u32 = 0x400A5000;
 macro_rules! reg {
     [ $offset:expr, $description:expr, $name:ident, "RW" ] => {
         #[allow(dead_code)]
-        pub const $name: Reg = Reg((USBC_BASE + $offset) as *mut u32);
+        pub const $name: Reg<u32> = Reg::new((USBC_BASE + $offset) as *mut u32);
+    };
+
+    [ $offset:expr, $description:expr, $name:ident, "RW", $t:ty ] => {
+        #[allow(dead_code)]
+        pub const $name: Reg<$t> = Reg::new((USBC_BASE + $offset) as *mut u32);
     };
 
     [ $offset:expr, $description:expr, $name:ident, "R" ] => {
         #[allow(dead_code)]
-        pub const $name: RegR = RegR((USBC_BASE + $offset) as *mut u32);
+        pub const $name: RegR<u32> = RegR::new((USBC_BASE + $offset) as *mut u32);
     };
 
     [ $offset:expr, $description:expr, $name:ident, "W" ] => {
         #[allow(dead_code)]
-        pub const $name: RegW = RegW((USBC_BASE + $offset) as *mut u32);
+        pub const $name: RegW<u32> = RegW::new((USBC_BASE + $offset) as *mut u32);
     };
 }
 
 macro_rules! regs {
     [ $offset:expr, $description:expr, $name:ident, "RW", $count:expr ] => {
         #[allow(dead_code)]
-        pub const $name: Regs = Regs((USBC_BASE + $offset) as *mut u32);
+        pub const $name: Regs<u32> = Regs::new((USBC_BASE + $offset) as *mut u32);
     };
 
     [ $offset:expr, $description:expr, $name:ident, "R", $count:expr ] => {
         #[allow(dead_code)]
-        pub const $name: RegsR = RegsR((USBC_BASE + $offset) as *mut u32);
+        pub const $name: RegsR<u32> = RegsR::new((USBC_BASE + $offset) as *mut u32);
     };
 
     [ $offset:expr, $description:expr, $name:ident, "W", $count:expr ] => {
         #[allow(dead_code)]
-        pub const $name: RegsW = RegsW((USBC_BASE + $offset) as *mut u32);
+        pub const $name: RegsW<u32> = RegsW::new((USBC_BASE + $offset) as *mut u32);
     };
 }
+
+// USBCON
 
 #[derive(Copy, Clone)]
 pub struct UsbCon(pub u32);
@@ -115,13 +170,18 @@ impl Not for UsbCon {
     }
 }
 
+impl From<u32> for UsbCon {
+    fn from(n: u32) -> Self {
+        UsbCon(n)
+    }
+}
+
 impl Into<u32> for UsbCon {
     fn into(self) -> u32 {
         self.0
     }
 }
 
-// Bits for USBCON
 pub const UIMOD: UsbCon = UsbCon(1 << 25);
 pub const USBE: UsbCon = UsbCon(1 << 15);
 pub const FRZCLK: UsbCon = UsbCon(1 << 14);
@@ -164,7 +224,7 @@ regs![0x05F0, "Pipe n Control Set Register", UPCONnSET, "W", 8];
 regs![0x0620, "Pipe n Control Clear Register", UPCONnCLR, "W", 8];
 regs![0x0650, "Pipe n IN Request Register", UPINRQn, "RW", 8];
 
-reg![0x0800, "General Control Register", USBCON, "RW"];
+reg![0x0800, "General Control Register", USBCON, "RW", UsbCon ];
 reg![0x0804, "General Status Register", USBSTA, "R"];
 reg![0x0808, "General Status Clear Register", USBSTACLR, "W"];
 reg![0x080C, "General Status Set Register", USBSTASET, "W"];
@@ -175,3 +235,36 @@ reg![0x0824, "IP Name Register 1", UNAME1, "R"];
 reg![0x0828, "IP Name Register 2", UNAME2, "R"];
 reg![0x082C, "USB Finite State Machine Status Register", USBFSM, "R"];
 reg![0x0830, "USB Descriptor address", UDESC, "RW"];
+
+pub trait AsWord {
+    fn as_word(self) -> u32;
+}
+
+impl AsWord for bool {
+    fn as_word(self) -> u32 { if self { 1 } else { 0 } }
+}
+
+// bitfield![UDCON_DETACH, UDCON, bool, 1, 8]
+
+pub struct BitField<T> {
+    reg: Reg<u32>,
+    bits: u32,
+    shift: u32,
+    phantom: PhantomData<*mut T>,
+}
+
+impl<T: AsWord> BitField<T> {
+    pub const fn new(reg: Reg<u32>, bits: u32, shift: u32) -> Self {
+        BitField { reg: reg, bits: bits, shift: shift, phantom: PhantomData }
+    }
+
+    #[inline]
+    pub fn write(self, val: T) {
+        let w = self.reg.read();
+        let mask = self.bits << self.shift;
+        let val_bits: u32 = (val.as_word() & self.bits) << self.shift;
+        self.reg.write(w & !mask | val_bits);
+    }
+}
+
+pub const UDCON_DETACH: BitField<bool> = BitField::new(UDCON, 1, 8);
