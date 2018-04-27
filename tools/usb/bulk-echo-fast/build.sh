e38545be58c924c9855d140b9bb9058fd68f570d
Daniel B Giffin <daniel@beech-grove.net>
2018-04-26 21:02:12 -0700

    wip

---
+++ tools/usb/bulk-echo-fast/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+$CC -I/usr/include/libusb-1.0/ main.c -lusb-1.0 
--- tools/usb/bulk-echo-fast/main.c
+++ tools/usb/bulk-echo-fast/main.c
@@ -15,17 +15,27 @@ typedef int bool;
 static const bool false = 0;
 static const bool true = 1;
 
-static const uint16_t TARGET_VENDOR_ID = 0x6667;
-static const uint16_t TARGET_PRODUCT_ID = 0xabcd;
-
 static struct pollfd fds[10];
 static const int timeout_never = -1;
 static size_t stdin_fdi;
 
+static const size_t input_bufsz = 100;
+static unsigned char input_buf[input_bufsz];
+static size_t input_buflen = 0;
 static size_t input_buf_avail(void);
+static bool input_buf_locked = false;
 static size_t read_input(void);
 
+static const uint16_t TARGET_VENDOR_ID = 0x6667;
+static const uint16_t TARGET_PRODUCT_ID = 0xabcd;
+
+unsigned char endpoint_bulk_in = 1 | 1 << 7;
+unsigned char endpoint_bulk_out = 2 | 0 << 7;
+
+static libusb_device_handle *zorp;
+
 void configure_device(void);
+
 static struct timeval timeval_zero = { 0, 0 };
 
 static bool done = false;
@@ -69,14 +79,49 @@ void configure_device(void) {
     }
     if (dev == NULL)
         error(1, 0, "Couldn't find target device");
+
+    if (libusb_open(dev, &zorp))
+        error(1, 0, "libusb_open");
+}
+
+void LIBUSB_CALL write_done(struct libusb_transfer *transfer) {
+    switch (transfer->status) {
+        case LIBUSB_TRANSFER_COMPLETED:
+            if (transfer->actual_length != transfer->length) {
+                error(1, 0, "short write");
+            }
+            input_buflen = 0;
+            input_buf_locked = false;
+            fprintf(stderr, "Wrote %d bytes to device\n", transfer->actual_length);
+            break;
+        default:
+            error(1, 0, "bad transfer status: %d", transfer->status);
+    }
+
+    libusb_free_transfer(transfer);
 }
 
 void handle_events(void) {
 
+    if (input_buflen > 0) {
+        // Write input buf to device
+
+        // Don't fiddle with input buffer while libusb is trying to send it
+        input_buf_locked = true;
+
+        int iso_packets = 0;
+        struct libusb_transfer* transfer = libusb_alloc_transfer(iso_packets);
+        libusb_fill_bulk_transfer(transfer, zorp, endpoint_bulk_out,
+                                  input_buf, input_buflen, write_done, NULL, 0);
+
+        if (libusb_submit_transfer(transfer))
+            error(1, 0, "submit");
+    }
+
     nfds_t nfds = 0;
 
     // Add stdin fd
-    bool poll_stdin = input_buf_avail() > 0;
+    bool poll_stdin = !input_buf_locked && input_buf_avail() > 0;
     if (poll_stdin) {
         fds[nfds].fd = 0;
         fds[nfds].events = POLLIN;
@@ -99,7 +144,7 @@ void handle_events(void) {
         nfds++;
     }
     libusb_free_pollfds(all_usb_fds);
-    fprintf(stderr, "Added %lu USB pollfds\n", nfds - 1);
+    fprintf(stderr, "\t(Added %lu USB pollfds)\n", nfds - 1);
 
     if (nfds == 0) {
         // Nothing to wait for
@@ -136,17 +181,12 @@ void handle_events(void) {
  * An input buffer
  */
 
-static const size_t input_bufsz = 100;
-static size_t input_buflen = 0;
-
 static size_t input_buf_avail(void) {
     return input_bufsz - input_buflen;
 }
 
 static size_t read_input(void) {
-    static char buf[input_bufsz];
-
-    ssize_t r = read(0, buf + input_buflen, input_bufsz - input_buflen);
+    ssize_t r = read(0, input_buf + input_buflen, input_bufsz - input_buflen);
     if (r < 0) {
         error(1, r, "read");
     }
